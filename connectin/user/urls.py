af264967a442b04ae3abaf0b2c88ed2ce47efcef
Anuj poudel <anujpoudel54@gmail.com>
2025-04-18 19:16:00 +0545

    add login url

--- connectin/user/urls.py
+++ connectin/user/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, UserCreate
+from .views import UserViewSet, UserCreateView, UserLoginView
 
 router = DefaultRouter()
 router.register('', UserViewSet, basename='user')
 
 urlpatterns = ([
-    path('register/', UserCreate.as_view(), name='register-user'),
+    path('register/', UserCreateView.as_view(), name='register-user'),
+    path('login/', UserLoginView.as_view(), name='login-user'),
     path('', include(router.urls)),
 ])
 
