4a1c32e1252ff1e9b88592ac7e1dd68423317b97
Anuj poudel <anujpoudel54@gmail.com>
2025-04-18 19:15:47 +0545

    rename serializers -> serializer

--- connectin/user/serializer.py
+++ connectin/user/serializer.py
@@ -7,10 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True,style={'input_type': 'password'})
     class Meta:
         model = User
-        fields = ['uid', 'full_name', 'email', 'contact_number', 'address', 'industry', 'company_name', 'password']
+        fields = ['uid', 'username', 'full_name', 'email', 'contact_number', 'address', 'industry', 'company_name', 'password']
     
     def create(self, validated_data: dict):
-        user = User.objects.create_user(username=validated_data['full_name'], **validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
 
     def validate_contact_number(self, value: str):
@@ -22,3 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
 
         return value
 
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
