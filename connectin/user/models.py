416c253121c5c936b70bc312391862334f2bdf41
Anuj poudel <anujpoudel54@gmail.com>
2025-04-18 16:57:44 +0545

    handle unique contraint failed for username

--- connectin/user/models.py
+++ connectin/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.db.models import F
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.db import IntegrityError
 
 from utils.models import BaseModel
 
@@ -9,6 +9,19 @@ from utils.models import BaseModel
 class User(BaseModel, AbstractUser):
     address = models.TextField()
     industry = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, unique=True)
     company_name = models.CharField(max_length=100)
-    contact_number = models.CharField(max_length=15)
+    contact_number = models.CharField(max_length=15, unique=True)
     full_name = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.full_name
+
+        try:
+            return super().save(*args, **kwargs)
+        except IntegrityError as e:
+
+            if "UNIQUE constraint" in e.args[0]:
+                self.username = self.full_name + str(self.uid)[:9]
+                return super().save(*args, **kwargs)
