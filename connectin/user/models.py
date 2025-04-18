b80e9d0cf34aeaebbd5a32d1aa4eeb44f069803e
Anuj poudel <anujpoudel54@gmail.com>
2025-04-18 16:19:57 +0545

    create user model

--- connectin/user/models.py
+++ connectin/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import F
+from django.contrib.auth.models import AbstractUser
+
+from utils.models import BaseModel
 
 # Create your models here.
+
+class User(BaseModel, AbstractUser):
+    address = models.TextField()
+    industry = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    full_name = models.CharField(max_length=100)
