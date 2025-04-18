8850a63051bcb7523f53fbc215c2269449b469cc
Anuj poudel <anujpoudel54@gmail.com>
2025-04-18 18:08:24 +0545

    create api view for registering usr

--- connectin/user/urls.py
+++ connectin/user/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import UserViewSet, UserCreate
 
 router = DefaultRouter()
 router.register('', UserViewSet, basename='user')
 
 urlpatterns = ([
+    path('register/', UserCreate.as_view(), name='register-user'),
     path('', include(router.urls)),
 ])
 
