78332768513c1cfd0dff4cd7d7e873329fb8239f
Anuj poudel <anujpoudel54@gmail.com>
2025-04-19 14:16:27 +0545

    feat: refactor ASGI configuration to support WebSocket routing with JWT authentication

--- connectin/connectin/asgi.py
+++ connectin/connectin/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for connectin project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectin.settings')
+from .routing import websocket_urlpatterns
+from user.middlewares.jwt_auth_middleware import JwtAuthMiddleware
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            JwtAuthMiddleware(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
