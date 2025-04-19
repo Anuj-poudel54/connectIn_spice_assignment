cac45819e8ffab67378003e385ff8871f67d82dc
Anuj poudel <anujpoudel54@gmail.com>
2025-04-19 14:17:43 +0545

    feat: add send_ws_notification task for WebSocket notifications

---
+++ connectin/notification/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from channels.layers import get_channel_layer
+
+from asgiref.sync import async_to_sync
+
+@shared_task
+def send_ws_notification(username: str, body: str):
+    channel_layer = get_channel_layer()
+    
+    async_to_sync(channel_layer.send)(
+        username,
+        {
+            "type": "notify",
+            "message": body,
+        }
+    )
