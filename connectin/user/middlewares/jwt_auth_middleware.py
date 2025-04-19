e08c49dd47674cb1d705cb312565bbfd0702d057
Anuj poudel <anujpoudel54@gmail.com>
2025-04-19 14:18:05 +0545

    feat: add JWT authentication middleware for user validation

---
+++ connectin/user/middlewares/jwt_auth_middleware.py
@@ -0,0 +1,52 @@
+
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import ExpiredTokenError, InvalidToken
+from jwt import decode as jwt_decode
+
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+
+User = get_user_model()
+
+@database_sync_to_async
+def get_user(user_id):
+    try:
+        return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return AnonymousUser()
+
+class JwtAuthMiddleware:
+   
+   
+   def __init__(self, app):
+        self.app = app
+
+   async def __call__(self, scope, receive, send):
+
+        headers = dict(scope["headers"])
+
+        auth_header: bytes = headers.get(b"authorization")
+
+        if not auth_header:
+            scope['user'] = AnonymousUser()
+            return await self.app(scope, receive, send)
+        
+        name, token = auth_header.decode().split(" ")
+        if name.lower() != "bearer":
+            raise ValueError(f"Invalid token name {name}")
+        
+        try:
+            UntypedToken(token)
+            decoded_data = jwt_decode(token.encode(), settings.SECRET_KEY, algorithms=["HS256"])
+            user_id = decoded_data['user_id']
+
+            scope['user'] = await get_user(user_id)
+
+        except Exception as  e:
+            print("ERROR: while validating jwt token in middleware\n", e)
+            scope['user'] = AnonymousUser()
+
+
+        return await self.app(scope, receive, send)
