5994a46f103946106e7df8cc06cfd0870dc1a3d5
Anuj poudel <anujpoudel54@gmail.com>
2025-04-20 16:42:15 +0545

    use db configs from env

--- connectin/connectin/settings.py
+++ connectin/connectin/settings.py
@@ -73,8 +73,12 @@ ASGI_APPLICATION = 'connectin.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DB_ENGINE", default="django.db.backends.sqlite3"),
+        'NAME': config("DB_NAME", default=BASE_DIR/'db.sqlite3'),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
