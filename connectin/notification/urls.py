7794ada5a9b58ca9be18f14c6aa3e8a32a43bda5
Anuj poudel <anujpoudel54@gmail.com>
2025-04-20 13:25:09 +0545

    create api url for listing and creating notification

--- connectin/notification/urls.py
+++ connectin/notification/urls.py
@@ -1,10 +1,14 @@
+from rest_framework.routers import DefaultRouter
 
-from django.urls import path
+from django.urls import path, include
 
 from .views import NotificationApiView
 
+router = DefaultRouter()
+router.register('', NotificationApiView)
 
 urlpatterns = ([
-    path('', NotificationApiView.as_view(), name='notifications'),
+    path('', NotificationApiView.as_view({"get": "list"}), name='list-notifications'),
+    path('create/', NotificationApiView.as_view({"post": "create"}), name='create-notifications'),
 ])
 
