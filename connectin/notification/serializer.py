b7bcc7bcf8b799ea0a7e60f6671c87fd65d251b3
Anuj poudel <anujpoudel54@gmail.com>
2025-04-20 13:24:38 +0545

    use user field in serializer for write only purpose

--- connectin/notification/serializer.py
+++ connectin/notification/serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from django.contrib.auth import get_user_model
+
 from .models import Notification
 
-class NotificationSerializer(serializers.ModelSerializer):
+UserModel = get_user_model()
 
+class NotificationSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=UserModel.objects.all() , write_only=True)
     class Meta:
         model = Notification
-        fields = ['uid', 'body']
+        fields = ['uid', 'body', 'user']
