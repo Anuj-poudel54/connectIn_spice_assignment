2895c4cef25dda70af5e7b4b1010e236fc23cba0
Anuj poudel <anujpoudel54@gmail.com>
2025-04-21 13:01:36 +0545

    add urls for connections

--- connectin/connection/urls.py
+++ connectin/connection/urls.py
@@ -6,6 +6,7 @@ from .views import ConnectionApiView
 
 
 urlpatterns = ([
+    path('accepted/', ConnectionApiView.as_view({"get": "list_connections"}), name='list-accepted-request'),
     path('send/', ConnectionApiView.as_view({"post": "send_connection"}), name='send-connection-request'),
     path('decide/', ConnectionApiView.as_view({"post": "decide_connection"}), name='decide-connection-request'),
     path('requests/', ConnectionApiView.as_view({"get": "list_request"}), name='list-connection-requests'),
